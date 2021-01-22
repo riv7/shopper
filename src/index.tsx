263adda1851ce806fa5f941e0a28228494eb31eb
Wolfram Gulde <wolfram.gulde@exxcellent.de>
2021-01-22 16:41:04 +0100

    feat(Firebase): add firebase

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzwoKs5sVWtAV_Br8XIZU3U4tbIHhUAuc",
+  authDomain: "shopper-backend-c590c.firebaseapp.com",
+  databaseURL: "https://shopper-backend-c590c-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "shopper-backend-c590c",
+  storageBucket: "shopper-backend-c590c.appspot.com",
+  messagingSenderId: "172446679872",
+  appId: "1:172446679872:web:8bec7969c429f01e94f0a5"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.database();
 
 ReactDOM.render(
   <React.StrictMode>
