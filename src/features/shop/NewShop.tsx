0865d81ea40630102c91737f21b29e5f9bdfff21
Wolfram Gulde <wolfram.gulde@exxcellent.de>
2021-02-12 17:29:54 +0100

    feat(Shop): Style new shop dialog

--- src/features/shop/NewShop.tsx
+++ src/features/shop/NewShop.tsx
@@ -1,4 +1,7 @@
-import { Button, createStyles, Grid, makeStyles, TextField, Theme } from '@material-ui/core';
+import { Box, Button, createStyles, Grid, makeStyles, TextField, Theme } from '@material-ui/core';
+import SaveIcon from '@material-ui/icons/Save';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
 import React, { FC, ReactElement } from 'react';
 import { useDispatch } from 'react-redux';
 import { addShop } from './shopSlice';
@@ -8,6 +11,9 @@ const useStyles = makeStyles((theme: Theme) =>
     root: {
       flexGrow: 1,
       marginTop: '25px'
+    },
+    textInput: {
+      marginLeft: '5px'
     }
   })
 );
@@ -17,18 +23,38 @@ const NewShop: FC = (): ReactElement => {
     const classes = useStyles();
     const dispatch = useDispatch();
     
-    const handleClick = () => {
+    const handleAddClick = () => {
         dispatch(addShop());
     }
+
+    const handleCancelClick = () => {
+      dispatch(addShop());
+  }
 
     return (
         <div className={classes.root}>
           <Grid container spacing={3}>
+            
             <Grid item xs={12}>
-                <TextField id="standard-basic" label="Standard" />
-                <Button variant="contained" color="secondary" onClick={() => handleClick()}>
-                    Add
-                </Button>
+              <Grid container justify="space-between" spacing={3}>
+                <Grid item>
+                  <IconButton color="secondary" aria-label="cancel">
+                    <CloseIcon />
+                  </IconButton>
+                </Grid>
+                <Grid item>
+                  <IconButton color="secondary" aria-label="save">
+                    <SaveIcon />
+                  </IconButton>
+                </Grid>
+              </Grid>
+              <Grid item xs={12}>
+                <Grid container className={classes.textInput} spacing={3}>
+                  <Grid item>
+                    <TextField id="standard-basic" label="Enter shop name ..." />
+                  </Grid>
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
         </div>
