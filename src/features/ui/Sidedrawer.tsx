12c7bf62c498c6796e850dd85f0e843f0c2e1a52
Wolfram Gulde <wolfram.gulde@exxcellent.de>
2022-02-16 21:46:23 +0100

    feat(Sidedrawer): add functionallity

--- src/features/ui/Sidedrawer.tsx
+++ src/features/ui/Sidedrawer.tsx
@@ -1,8 +1,7 @@
 import React, { FC, SetStateAction } from 'react';
 import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
@@ -11,15 +10,27 @@ import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 import { Dispatch } from 'react';
+import { Card, CardHeader } from '@material-ui/core';
+import firebase from 'firebase';
+import { activeTeam, Team } from '../team/teamSlice';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
-const useStyles = makeStyles({
-  list: {
-    width: 250,
-  },
-  fullList: {
-    width: 'auto',
-  },
-});
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    list: {
+      width: 250,
+    },
+    fullList: {
+      width: 'auto',
+    },
+    cardBackground: {
+      background: theme.palette.secondary.dark
+    }
+  }),
+);
 
 type SidedrawerProps = {
   drawerOpenState: [boolean, Dispatch<SetStateAction<boolean>>]
@@ -28,7 +39,20 @@ type SidedrawerProps = {
 const Sidedrawer:FC<SidedrawerProps> = ({drawerOpenState}) =>  {
 
   const classes = useStyles();
+  const history = useHistory();
   const [drawerOpen, setDrawerOpen] = drawerOpenState;
+  const actTeam: Team | undefined = useSelector(activeTeam);
+
+  const teamName = () => actTeam ? actTeam.name : ''
+  const owner = () => actTeam ? actTeam.ownerId === firebase.auth().currentUser!.uid ? ' (owner)' : '' : '';
+
+  const handleTeamClick = () => {
+    history.push('team');
+  };
+
+  const handleLogoutClick = () => {
+    firebase.auth().signOut();
+  }
 
   const toggleDrawer = (open: boolean) => (
     event: React.KeyboardEvent | React.MouseEvent,
@@ -53,22 +77,33 @@ const Sidedrawer:FC<SidedrawerProps> = ({drawerOpenState}) =>  {
       onClick={toggleDrawer(false)}
       onKeyDown={toggleDrawer(false)}
     >
+      <Card className={classes.cardBackground}>
+        <CardHeader title="shopper"/>
+      </Card>
+      {/* <Typography>shopper</Typography> */}
+      {/* <Divider /> */}
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
+        <ListItem key="user">
+          <ListItemIcon><InboxIcon /></ListItemIcon>
+          <ListItemText primary={firebase.auth().currentUser!.displayName} />
+        </ListItem>
+        <ListItem button onClick={handleLogoutClick} key="logout">
+          <ListItemIcon><MailIcon /></ListItemIcon>
+          <ListItemText primary="Logout" />
+        </ListItem>
       </List>
       <Divider />
       <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+        <ListItem button onClick={handleTeamClick} key="team">
+          <ListItemIcon><MailIcon /></ListItemIcon>
+          <ListItemText primary={teamName() + owner()} />
+        </ListItem>
+        {/* {['All mail', 'Trash', 'Spam'].map((text, index) => (
           <ListItem button key={text}>
             <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
-        ))}
+        ))} */}
       </List>
     </div>
   );
